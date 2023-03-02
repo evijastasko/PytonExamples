37a15bebaac1d1489dd00e0205c97c6884c9dc54
evijastasko <evija.stasko@gmail.com>
2023-03-02 18:51:37 +0200

    2

--- Devops/sgt/Homework2/Homework2.py
+++ Devops/sgt/Homework2/Homework2.py
@@ -1,1 +1,11 @@
 word = input("Please input a word:")
+for x in range(len(word) -1, -1, -1):
+    print(word[x], end="")
+    #end means that all letters will be in one line and not below each other
+print("\n")
+
+print(x)
+
+
+
+
