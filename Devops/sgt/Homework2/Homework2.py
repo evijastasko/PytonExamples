c7158a418bfa3b4dd2fb35cf2d3b0bb30d8876f0
evijastasko <evija.stasko@gmail.com>
2023-03-02 19:48:06 +0200

    3

--- Devops/sgt/Homework2/Homework2.py
+++ Devops/sgt/Homework2/Homework2.py
@@ -1,10 +1,32 @@
+#Task1
+def my_function (*numbers):
+    return max(numbers)
+print(my_function(14, 7, 3))
+
+#Task2
 word = input("Please input a word:")
 for x in range(len(word) -1, -1, -1):
     print(word[x], end="")
-    #end means that all letters will be in one line and not below each other
+#end means that all letters will be in one line and not below each other
 print("\n")
 
 print(x)
+
+#Task3
+
+def sum_numbers(*numbers: int) ->int:
+    """"Sums all the numbers in the list"""
+    return sum(numbers)
+print(sum_numbers(1.1, 2.2, 5.5))
+
+#word = input("Please input a word:")
+#for x in range(len(word) -1, -1, -1):
+    #print(word[x], end="")
+    #end means that all letters will be in one line and not below each other
+#print("\n")
+#print empty line
+
+#print(x)
 
 
 
