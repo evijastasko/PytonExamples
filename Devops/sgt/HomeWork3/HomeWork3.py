6281800191885f1af1adbafd3fd40d60fab9950d
evijastasko <evija.stasko@gmail.com>
2023-03-06 18:47:18 +0200

    4

--- Devops/sgt/HomeWork3/HomeWork3.py
+++ Devops/sgt/HomeWork3/HomeWork3.py
@@ -1,6 +1,6 @@
 # Subtrat five days from the current date
 import datetime
 
-today = today = datetime.date.today()
+today = datetime.date.today()
 date = today - datetime.timedelta(days=5)
 print(date)
