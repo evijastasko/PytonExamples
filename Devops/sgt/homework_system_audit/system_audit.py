46308677aaa614474d16f17b96b540c79371ab8b
evijastasko <evija.stasko@gmail.com>
2023-03-09 13:40:49 +0200

    1

--- Devops/sgt/homework_system_audit/system_audit.py
+++ Devops/sgt/homework_system_audit/system_audit.py
@@ -10,21 +10,18 @@ surname = input("What is your last name? ")
 ID = input("What is your ID number? ")
 
 directory = today
-parent_dir = "/home/sgtuser/homework5"
+parent_dir = "/home/sgtuser/Documents/Devops/sgt/homework_system_audit"
 path = os.path.join(parent_dir, str(directory))
 
-#def make_directory() :
 if not os.path.exists(path):
     os.makedirs(path)
 
-filename = ID + ".txt"
-#def create_file() :
+filename = os.path.join(path,ID + ".txt")
 with open(filename, "a") as f:
     f.write(name + surname + ID)
 
-#def output() :
-for file in os.listdir(path):
-    with open(path), "r") as f:
-        print(f.read())
+print("Files in directory: ", os.listdir(path))
+
+
 
 
