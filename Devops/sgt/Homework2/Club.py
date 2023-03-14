4508b34b93c53f9887ae63b6fc2d26bedb5f3dfd
evijastasko <evija.stasko@gmail.com>
2023-03-14 23:26:59 +0200

    Club

---
+++ Devops/sgt/Homework2/Club.py
@@ -0,0 +1,7 @@
+a = input("What is your name?")
+b = input("What is your age?")
+b=int(b)
+if b<18:
+    print("You are not old enough to enter the club")
+else:
+    print(a+ ", " + "Welcome to the club!")
