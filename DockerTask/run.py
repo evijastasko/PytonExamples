de6b171800876e7ac4ca84f8ae70650ba34a3d16
evijastasko <evija.stasko@gmail.com>
2023-04-03 23:47:38 +0300

    ..

---
+++ DockerTask/run.py
@@ -0,0 +1,40 @@
+import os
+import time
+import mysql.connector
+
+def connect_to_mysql():
+    start_time = time.time()
+    timeout = 60
+
+    while time.time() - start_time < timeout:
+        try:
+            connection = mysql.connector.connect(
+                host="myssql",
+                user="edgars",
+                password="qwerty",
+                database="db"
+            )
+            print("Connected to MySQL")
+            return connection
+        except mysql.connector.Error as e:
+            print(f"Error connecting to MySQL: {e}")
+            time.sleep(5)
+
+    raise Exception(f"Could not connect to MySQL within {timeout} seconds")
+
+if name == "main":
+    try:
+        while True:
+            with connect_to_mysql() as connection:
+                cursor = connection.cursor()
+                try:
+                    cursor.execute("SELECT VERSION()")
+                    result = cursor.fetchone()
+                    print(f"!!MySQL version!!: {result}")
+                finally:
+                    cursor.close()
+
+            time.sleep(5)
+    except KeyboardInterrupt:
+        print("Stopping script")
+pip freeze > requrirements.txt
