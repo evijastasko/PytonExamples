767515506ae49e20abc392dec1066834e61b2457
evijastasko <evija.stasko@gmail.com>
2023-04-03 22:33:35 +0300

    1

---
+++ Devops/sgt/homework_system_audit/insert_data.py
@@ -0,0 +1,26 @@
+from mysql.connector import connect, Error
+from decouple import config
+
+try:
+    configValue = {
+        "host": "localhost",
+        "user": config("username"),
+        "password": config("password"),
+        "database": "system_audit"
+    }
+    with connect(**configValue) as connection:
+        insert_Audit_log = """
+            INSERT INTO Audit_log
+            (date,time)
+            VALUES (%s, %s)
+            """
+        audit_log_data = [
+            ("01.04.2023", "12:10"),
+            ("02.04.2023", "13:10")
+]
+    with connection.cursor() as cursor:
+        cursor.executemany(insert_Audit_log, audit_log_data)
+        connection.commit()
+
+except Error as e:
+    print(e)
